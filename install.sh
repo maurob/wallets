b0fab45b3993b69fac63ebdd508cbaf957e6cf5b
Mauro Bruni <maumarbru@gmail.com>
2024-03-09 16:03:39 -0300

    Freeze versions in .lock

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ repo_dir=$(dirname "$(readlink -f "$0")")
 python3 -m venv --prompt wallets $repo_dir/venv
 source $repo_dir/venv/bin/activate
 pip install --upgrade pip
-pip install .
+pip install -r requirements.lock
 for interpreter in bash zsh fish pwsh; do
     wallets --install-completion $interpreter 2> /dev/null
 done
