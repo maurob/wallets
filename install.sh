dbffe56e764a580f4c2acb30587edf7a4fa325d8
Mauro Bruni <maumarbru@gmail.com>
2024-03-09 15:39:00 -0300

    Also upgrade pip

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 repo_dir=$(dirname "$(readlink -f "$0")")
 python3 -m venv --prompt wallets $repo_dir/venv
 source $repo_dir/venv/bin/activate
+pip install --upgrade pip
 pip install .
 for interpreter in bash zsh fish pwsh; do
     wallets --install-completion $interpreter 2> /dev/null
