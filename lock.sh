17a1bed9fab19b731c530890f67c203679bce7af
Mauro Bruni <maumarbru@gmail.com>
2024-03-19 16:43:25 -0300

    Improve comments

--- lock.sh
+++ lock.sh
@@ -3,7 +3,7 @@
 if [ -n "$VIRTUAL_ENV" ]; then
     pip freeze | sed '/^wallets/ s/.*/./' > requirements.lock
     # It also replaces the local project python package with the currentpath ".",
-    # to avoid local filesystem referencies.
+    # to avoid absolute local filesystem referencies.
 else
-    echo "You have to run this script inside a virtualenv."
+    echo "You have to run this script with an activated virtualenv."
 fi
